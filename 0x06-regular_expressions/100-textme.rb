00580e7991d0b25fb62b203dc81bef43e5c0fdd5
munenecarl <munenecarl@gmail.com>
2023-10-03 23:20:11 +0300

    correction

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,29 +1,25 @@
 #!/usr/bin/env ruby
 
 # Define the regular expression pattern to match sender, receiver, and flags
-pattern = /^(?:Message from (\S+) to (\S+):|Text from (\S+) to (\S+):)\s*(.*?)\s*$/i
+pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/i
 
-# Check if a log file argument is provided
+# Check if a log entry argument is provided
 if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <log_file>"
+  puts "Usage: #{$PROGRAM_NAME} <log_entry>"
   exit 1
 end
 
-# Get the log file path from the command-line arguments
-log_file_path = ARGV[0]
+# Get the log entry from the command-line arguments
+log_entry = ARGV[0]
 
-# Check if the log file exists
-if File.exist?(log_file_path)
-  # Open and read the log file line by line
-  File.foreach(log_file_path) do |line|
-    match_data = line.match(pattern)
-    if match_data
-      sender = match_data[1] || match_data[3]
-      receiver = match_data[2] || match_data[4]
-      flags = match_data[5].scan(/#\w+/).join(',') # Extract and join flags
-      puts "#{sender},#{receiver},#{flags}"
-    end
-  end
+# Extract sender, receiver, and flags using the regular expression
+match_data = log_entry.match(pattern)
+
+if match_data
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
+  puts "#{sender},#{receiver},#{flags}"
 else
-  puts "The log file '#{log_file_path}' does not exist."
+  puts "Invalid log entry format."
 end
