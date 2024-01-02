08e6ed18ea5f359a1f6f21ba074f5005770fce0b
munenecarl <munenecarl@gmail.com>
2024-01-03 01:53:06 +0300

    Refactor employee todo list retrieval and CSV export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """Exports employees' tasks to a csv file"""
 
+import csv
 import requests
 import sys
-import csv
 
 def get_employee_todo_list(employee_id):
-	employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+	employee_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+				  .format(employee_id))
 	employee = employee_response.json()
-	todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+	todos_response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+			       .format(employee_id))
 	todos = todos_response.json()
 
-	with open(f'{employee_id}.csv', 'w', newline='') as file:
+	with open("{}.csv"
+	   .format(employee_id), 'w', newline='') as file:
 		writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 		writer.writerow(["TASK_COMPLETED_STATUS", "TASK_TITLE"])
 		for todo in todos:
