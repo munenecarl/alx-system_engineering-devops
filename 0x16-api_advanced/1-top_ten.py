5aaf6dc4f7a47913c20b943b25eb33cb85626ccd
munenecarl <munenecarl@gmail.com>
2024-03-01 03:34:26 +0300

    Initial commit for web server monitoring task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-"""Gets the top ten posts for a given subreddit"""
+"""Module to print the top 10 hot posts of a subreddit"""
 
 import requests
-import sys
+
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the top ten hot posts for a given subreddit.
+    """Prints the top 10 hot posts of a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    response = requests.get(url, headers=headers).json()
 
-    Parameters:
-    subreddit (str): The name of the subreddit to query.
-
-    Prints:
-    str: The title of each hot post. If the subreddit is invalid, prints None.
-    """
-    
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10', headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        posts = response.json()['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass the subreddit to be checked.")
-    else:
-        print("{:d}".format(top_ten(sys.argv(1))))
+    if response.get("error") == 404:
+        print("None")
+        return 0
+    for post in response.get("data").get("children"):
+        print(post.get("data").get("title"))
