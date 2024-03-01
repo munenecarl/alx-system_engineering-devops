67ad3860b1e772309a2ec957f55d5619a6175e09
munenecarl <munenecarl@gmail.com>
2024-03-01 03:41:10 +0300

    Initial commit for web server monitoring task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Module to print the top 10 hot posts of a subreddit"""
-
-import requests
+"""Module to query Reddit API"""
 
 
 def top_ten(subreddit):
-    """Prints the top 10 hot posts of a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    response = requests.get(url, headers=headers).json()
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    if response.get("error") == 404:
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False,
+    )
+    if sub_info.status_code >= 300:
         print("None")
-        return 0
-    for post in response.get("data").get("children"):
-        print(post.get("data").get("title"))
+    else:
+        [
+            print(child.get("data").get("title"))
+            for child in sub_info.json().get("data").get("children")
+        ]
